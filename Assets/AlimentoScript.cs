d80b22714019bd7e35a1835cdb5bb72cbdc08688
Ian Baron <ianezebaron@gmail.com>
2024-06-06 12:09:40 -0300

    Alimento error script para consumir

---
+++ Assets/AlimentoScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlimentoScript : MonoBehaviour
+{
+
+
+
+    // Start is called before the first frame update
+    public int NivelHambre;
+
+    void TomarAlimento(int ValorAlimentario)
+    {
+        NivelHambre -= ValorAlimentario;
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        AlimentoScript alimento;
+        alimento = other.GetComponent<AlimentoScript>();
+    }
+}
---
+++ Assets/Scripts/AlimentoScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlimentoScript : MonoBehaviour
+{
+   
+    public int nivelhambre;
+
+    void TomarAlimento(int ValAlimentario)
+    {
+        nivelhambre -= ValAlimentario;
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        AlimentoScript alimento;
+        alimento = other.GetComponent<AlimentoScript>();
+        if (alimento)
+        {
+            TomarAlimento (alimento.ValAlimentario);
+        }
+    }
+}
+}
